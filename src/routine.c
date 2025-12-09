8f36e062794bbc77036341587ebb8157b43f7095
hikaru <ichi-hikaru@i.softbank.jp>
2025-12-09 23:26:08 +0900

    fix some test case is executed correctly.

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: ichikawahikaru <ichikawahikaru@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/28 00:00:00 by ichikawahik       #+#    #+#             */
-/*   Updated: 2025/10/17 18:40:28 by ichikawahik      ###   ########.fr       */
+/*   Updated: 2025/12/09 18:17:39 by ichikawahik      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@ static void	one_philo_edge(t_philo *ph)
 	t_state	*st;
 
 	st = ph->state;
+	pthread_mutex_lock(&st->state_mutex);
+	ph->last_meal_ms = now_ms();
+	pthread_mutex_unlock(&st->state_mutex);
 	pthread_mutex_lock(&st->forks[ph->left_fork_index]);
 	log_action(st, ph->id, "has taken a fork", false);
 	while (!is_sim_over(st))
@@ -41,7 +44,6 @@ static int	check_meals_done(t_philo *ph, t_state *st)
 
 void	philo_sleep_and_think(t_philo *ph)
 {
-	int		think_time;
 	t_state	*st;
 
 	st = ph->state;
@@ -49,12 +51,7 @@ void	philo_sleep_and_think(t_philo *ph)
 	sleep_ms_interruptible(st, st->config.time_to_sleep_ms);
 	log_action(st, ph->id, "is thinking", false);
 	if (st->config.number_of_philosophers % 2 == 1)
-	{
-		think_time = st->config.time_to_eat_ms * 2
-			- st->config.time_to_sleep_ms;
-		if (think_time > 0)
-			usleep(think_time * 500);
-	}
+		usleep(500);
 }
 
 static void	philo_loop(t_philo *ph, t_state *st)
@@ -77,16 +74,13 @@ void	*philosopher_routine(void *arg)
 
 	ph = (t_philo *)arg;
 	st = ph->state;
+	if (st->config.number_of_philosophers == 1)
+		return (one_philo_edge(ph), NULL);
+	if (ph->id % 2 == 0)
+		usleep(st->config.time_to_eat_ms * 500 + 100);
 	pthread_mutex_lock(&st->state_mutex);
 	ph->last_meal_ms = now_ms();
 	pthread_mutex_unlock(&st->state_mutex);
-	if (st->config.number_of_philosophers == 1)
-	{
-		one_philo_edge(ph);
-		return (NULL);
-	}
-	if (ph->id % 2 == 0)
-		usleep(st->config.time_to_eat_ms * 500);
 	philo_loop(ph, st);
 	return (NULL);
 }
